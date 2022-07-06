ca0c33985a8977ce41f375ba18d90cae559d64f3
Jose Bartholomeu <joseb.sampaio@gmail.com>
2022-07-06 17:41:15 -0300

    Adicionando DB

---
+++ src/app/app-routing.modules.ts
@@ -0,0 +1,18 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, RouterOutlet, Routes } from "@angular/router";
+import { FirstComponentComponent } from "./components/first-component/first-component.component";
+import { ListRenderComponent } from "./components/list-render/list-render.component";
+
+const routes: Routes = [
+    {path: '', component: FirstComponentComponent},
+    {path: 'List', component: ListRenderComponent},
+
+]
+
+@NgModule({
+    declarations: [],
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule {}
